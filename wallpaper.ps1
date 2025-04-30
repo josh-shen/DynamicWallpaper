63a94c144ae2e5f6dcfe63731611de4c3e8d9a70
Jash <joshausneh@gmail.com>
2025-04-30 11:01:39 -0500

    Better error logging

--- wallpaper.ps1
+++ wallpaper.ps1
@@ -1,7 +1,12 @@
 . "$PSScriptRoot\config.ps1"
 
+Function Write-Log($Message) {
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    "$timestamp - $Message" | Out-File -FilePath "$PSScriptRoot\log.txt" -Append
+}
+
 Function Set-WallPaper($Image) {
- 
+
 Add-Type -TypeDefinition @" 
 using System; 
 using System.Runtime.InteropServices;
@@ -15,16 +20,27 @@ public class Params
                                                    Int32 fuWinIni);
 }
 "@  
-    $ret = [Params]::SystemParametersInfo(0x14, 0, $Image, 0)
- 
+
+    $SPI_SETDESKWALLPAPER = 0x0014
+    $UpdateIniFile = 0x01
+    $SendChangeEvent = 0x02
+  
+    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
+  
+    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
+    return $ret
 }
 
 $t_minute = (Get-Date).Hour * 60 + (Get-Date).Minute
 
-# run Python script to setup time markers
-python setup.py
-
-$times = Get-Content -Path "$PSScriptRoot\times.txt"
+try {
+    # run Python script to setup time markers
+    python setup.py
+    $times = Get-Content -Path "$PSScriptRoot\times.txt"
+} catch {
+    Write-Log "ERROR: Failed to run Python script"
+    exit 1
+}
 
 $wallpaper_path = switch ($t_minute) {
     {$_ -ge $times[0] -and $_ -lt $times[1]} {
@@ -53,14 +69,21 @@ $wallpaper_path = switch ($t_minute) {
     }
 }
 
+# Check if image exists at path
+if (-not (Test-Path -Path $wallpaper_path -PathType Leaf)) {
+    Write-Log "ERROR: No image was found at path: $wallpaper_path"
+    exit 1
+}
+
+$result = Set-Wallpaper -Image $wallpaper_path
+
 # get current wallpaper image path
 $bytes = (New-Object -ComObject WScript.Shell).RegRead("HKEY_CURRENT_USER\Control Panel\Desktop\TranscodedImageCache"); 
 $current_wallpaper = ([System.Text.Encoding]::Unicode.GetString($bytes[24..($bytes.length-1)]) -split "\0")[0]
 
-Set-Wallpaper -Image $wallpaper_path
-
-$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-$logMessage = "$timestamp - Changed wallpaper to: $wallpaper_path - Previous wallpaper: $current_wallpaper"
-$logMessage | Out-File -FilePath "$PSScriptRoot\log.txt" -Append
-
-# TODO clar contents of log.txt every once in while
+# log results
+if ($result -eq 0) {
+    Write-Log "ERROR: Wallpaper change failed"
+} else if ($result -eq 1 -and $current_wallpaper -ne $wallpaper_path) {
+    Write-Log "${result}: Wallpaper changed from $current_wallpaper to $wallpaper_path"
+}
